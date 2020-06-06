1f54eee5c1b90512235d8c30f69f873183217033
neesraut <neesraut1@gmail.com>
2020-06-06 14:53:25 +1000

    Project added

---
+++ SDWAN/add.py
@@ -0,0 +1,13 @@
+
+class Add:
+    def __init__(mysillyobject,x,y):
+        mysillyobject.x = x
+        mysillyobject.y = y
+
+    def greeting(a):
+        print("Hello, the first parameter passed is "+a.x +" and second parameter is "+ a.y)
+
+
+
+
+
---
+++ SDWAN/addValues.py
@@ -0,0 +1,10 @@
+# from add import Add
+#
+# p1 = Add('anx', 'bp')
+# print(p1.greeting())
+
+#comment here
+print("Hello , World")
+x, y, z = 2, 3, 4
+print(x)
+
